0e375e9a7680394684baa441b7568dc0cf35eb34
Florian Deschambres <floriandeschambres@MacBook-Air-de-Florian.local>
2025-10-29 12:18:58 +0100

    init commit

---
+++ BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/OnSeRetrouveOuApplication.java
@@ -0,0 +1,13 @@
+package com.backend.OnSeRetrouveOu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnSeRetrouveOuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnSeRetrouveOuApplication.class, args);
+	}
+
+}
---
+++ BackendOnSeRetrouveOu/src/test/java/com/backend/OnSeRetrouveOu/OnSeRetrouveOuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.OnSeRetrouveOu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnSeRetrouveOuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
