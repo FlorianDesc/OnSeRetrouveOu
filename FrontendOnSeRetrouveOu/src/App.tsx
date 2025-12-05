66f3788972138162db55b7c17463e19717bc7a2f
florianDesc <deschambresflorian@gmail.com>
2025-12-05 17:19:27 +0100

    add all dependencies

--- FrontendOnSeRetrouveOu/src/App.tsx
+++ FrontendOnSeRetrouveOu/src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="text-red-500">
+      <Button>test</Button>
+      App
+    </div>
+  );
 }
