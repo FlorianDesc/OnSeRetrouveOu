1bd11cbe5d5708eadad2195d2bd261e912036dbf
florianDesc <deschambresflorian@gmail.com>
2025-12-30 19:54:13 +0100

    create activity on backend working

--- BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/service/ActivityService.java
+++ BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/service/ActivityService.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.backend.OnSeRetrouveOu.dto.CreateActivityRequest;
 import com.backend.OnSeRetrouveOu.model.Activity;
+import com.backend.OnSeRetrouveOu.model.User;
 import com.backend.OnSeRetrouveOu.repository.ActivityRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,5 +21,17 @@ public class ActivityService {
     public Page<Activity> getAllActivities(int page, int size){
         Pageable pageable = PageRequest.of(page, size);
         return activityRepository.findAll(pageable);
+    }
+
+    public Activity createActivity(CreateActivityRequest request, User creator) {
+        Activity activity = new Activity();
+        activity.setTitle(request.getTitle());
+        activity.setDescription(request.getDescription());
+        activity.setLocation(request.getLocation());
+        activity.setDateActivity(request.getDateActivity());
+        activity.setMaxParticipants(request.getMaxParticipants());
+        activity.setCreator(creator);
+        
+        return activityRepository.save(activity);
     }
 }
