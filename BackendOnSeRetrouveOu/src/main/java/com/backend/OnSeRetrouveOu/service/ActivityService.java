7839c132e1b205011881e6f6d9c76f41ddd7b80e
florianDesc <deschambresflorian@gmail.com>
2025-12-30 22:43:56 +0100

    add missing file

--- BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/service/ActivityService.java
+++ BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/service/ActivityService.java
@@ -3,6 +3,7 @@ package com.backend.OnSeRetrouveOu.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.backend.OnSeRetrouveOu.dto.CreateActivityRequest;
@@ -19,7 +20,7 @@ public class ActivityService {
     private final ActivityRepository activityRepository;
 
     public Page<Activity> getAllActivities(int page, int size){
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
         return activityRepository.findAll(pageable);
     }
 
