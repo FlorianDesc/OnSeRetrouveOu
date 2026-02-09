7eef6dbb6485fb159ea3af41f4d1bdfdf5545291
florianDesc <deschambresflorian@gmail.com>
2026-02-09 09:48:30 +0100

    add profil page

--- BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/dto/UserResponse.java
+++ BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/dto/UserResponse.java
@@ -14,5 +14,6 @@ public class UserResponse {
   private String firstname;
   private String lastname;
   private String email;
+  private String profileImage;
   private LocalDateTime createdAt;
 }
--- BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/model/User.java
+++ BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/model/User.java
@@ -28,6 +28,9 @@ public class User {
     @Column(unique = true)
     private String email;
 
+    @Column(name = "profile_image")
+    private String profileImage;
+
     @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
 }
