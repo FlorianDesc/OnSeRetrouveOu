4b524cf84f983ef657d223579ef05ff33a3d3bda
florianDesc <deschambresflorian@gmail.com>
2026-04-12 15:27:51 +0200

    fix delete activity bug and wrong image showing

--- BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/repository/CollaborativeListItemRepository.java
+++ BackendOnSeRetrouveOu/src/main/java/com/backend/OnSeRetrouveOu/repository/CollaborativeListItemRepository.java
@@ -1,11 +1,20 @@
 package com.backend.OnSeRetrouveOu.repository;
 
-import com.backend.OnSeRetrouveOu.model.CollaborativeListItem;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.backend.OnSeRetrouveOu.model.CollaborativeListItem;
 
 public interface CollaborativeListItemRepository extends JpaRepository<CollaborativeListItem, Long> {
     List<CollaborativeListItem> findByActivityId(Long activityId);
     Optional<CollaborativeListItem> findByIdAndActivityId(Long id, Long activityId);
+    
+    @Modifying
+    @Query("DELETE FROM CollaborativeListItem c WHERE c.activity.id = :activityId")
+    void deleteByActivityId(@Param("activityId") Long activityId);
 }
